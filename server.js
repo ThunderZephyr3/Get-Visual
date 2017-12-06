8809d477814cd76f25a6b5dcf06f90a7eb280656
Ringo Yip <ringoyip0901@gmail.com>
2017-12-05 20:33:25 -0800

    833pm

--- server.js
+++ server.js
@@ -2,17 +2,24 @@ let express = require('express');
 let bodyParser = require('body-parser');
 let morgan = require('morgan');
 let pg = require('pg');
-const { Pool, Client } = require('pg')
+// const { Pool, Client } = require('pg')
 let cors = require('cors');
 
 const PORT = 3000;
-const connectionString = 'postgres://lntjpzrb:WqEJbb3UKzjRlZ9SfCFO7GQ4itHsYl9j@baasu.db.elephantsql.com:5432/lntjpzrb';
+// const connectionString = 'postgres://lntjpzrb:WqEJbb3UKzjRlZ9SfCFO7GQ4itHsYl9j@baasu.db.elephantsql.com:5432/lntjpzrb';
+let pool = new pg.Pool ({
+  user: "postgres",
+  database: "ringoyip0901",
+  password: '0901',
+  host: 'localhost',
+  port: 3001, 
+  max: 10,
+});
 
 let app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(morgan('dev'));
 
 app.use(function(request, response, next) {
@@ -36,52 +43,50 @@ app.use(function(request, response, next) {
 //   // password: 'WqEJbb3UKzjRlZ9SfCFO7GQ4itHsYl9j'
 // });
 
-let pool = new Pool {
-  user: "postgres",
-  database: "ringoyip0901",
-  password: 0901,
-  host: 'localhost',
-  port: 3001, 
-  max: 10
-}
 
 
 //this is when the user starts entering information on the CLIENT card
-app.post("/submit", function(req, res) {
-  var c_id = req.body.c_id; 
-  var c_request = req.body.c_request;
+app.post("/api/submit", function(req, res) {
+  console.log('line 50', req.body)
+  console.log("line 51",req.body.id)
+  var id = req.body.id; 
+  var request = req.body.request;
   var c_path = req.body.c_path; 
   var c_component = req.body.c_component;
   var c_purpose = req.body.c_purpose;
   var c_moreInfo = req.body.c_moreInfo;
 
-  var s_id = req.body.s_id; 
-  var s_request = req.body.s_request;
+  // var s_id = req.body.s_id; 
+  // var s_request = req.body.s_request;
   var s_path = req.body.s_path; 
   var s_url = req.body.s_url;
   var s_purpose = req.body.s_purpose;
   var s_moreInfo = req.body.s_moreInfo;
 
-  let values = [id, request, c_path, c_component, c_purpose, c_moreInfo,s_path, s_url, s_purpose, s_moreInfo];
+  let values = [id, request, c_path, c_component, c_purpose, c_moreInfo, s_path, s_url, s_purpose, s_moreInfo];
 
   pool.connect((err, db, done) => {
       if (err) {
+        console.log("error at line 70")
         return res.status(400).send(err);
       }
       else {
-        db.query('INSERT INTO CARDS (id, request, c_path, c_component, c_purpose, c_moreInfo, s_path, s_url, s_purpose, s_moreInfo) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);', [...c_values],(err,response) => {
+        db.query('INSERT INTO cards (id, request, "c_path", "c_component", "c_purpose", "c_moreInfo", "s_path", "s_url", "s_purpose", "s_moreInfo") VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', [...values],(err,table) => {
           done();
-          console.log(err, response);
+          // console.log(err, res);
           if (err) {
-            return response.status(400).send(err);
+            console.log('error at line 78')
+            // console.log(err)
+            return res.status(400).send(err);
           }
           else {
-            response.status(201).send({message: "data inserted"})
+            res.status(201).send({message: "data inserted"})
+            // db.end();
           }
         })
       }
   });
-}
+});
 
 
 //under componentDidMount
@@ -91,7 +96,7 @@ app.get('/receive', function(request, response) {
       return response. status(400).send(err);
     }
     else {
-      db.query('SELECT * from CARDS', function(err, table) {
+      db.query('SELECT * from cards', function(err, table) {
         done();
         if(err) {
           console.log("error: ", err);
@@ -169,72 +174,6 @@ app.get('/receive', function(request, response) {
 //   client.end()
 // })
 
-
-// app.delete('/api/remove/:id', function(request, response) {
-//   var id = request.params.id;
-//   pool.connect(function(err, db, done) {
-//     if(err) {
-//       return response.status(400).send(err);
-//     }
-//     else {
-//       db.query('DELETE FROM recipes WHERE id = $1', [Number(id)], function(err, result) {
-//         done();
-//         if(err) {
-//           return response.status(400).send(err);
-//         }
-//         else {
-//           return response.status(200).send({message: 'success in deleting record'});
-//         }
-//       })
-//     }
-//   })
-// })
-
-// app.get('/api/recipes', function(request, response) {
-//   pool.connect(function(err, db, done) {
-//     if(err) {
-//       return response.status(400).send(err);
-//     }
-//     else {
-//       db.query('SELECT * FROM recipes', function(err, table) {
-//         done();
-//         if(err) {
-//           console.log('table', table);
-//           return response.status(400).send(err);
-//         }
-//         else {
-//           return response.status(200).send(table.rows);
-//         }
-//       })
-//     }
-//   })
-// })
-
-// app.post('/api/new-recipe', function(request, response) {
-//   var recipe_name = request.body.recipe_name;
-//   // var continent_name = request.body.continent_name;
-//   var ingredients_name = request.body.ingredients_name;
-//   var description_name = request.body.description_name;
-//   var id = request.body.id;
-//   let values = [recipe_name, ingredients_name, description_name, id];
-//   // let values = [recipe_name, continent_name, id];
-//   pool.connect((err, db, done) => {
-//   if(err) {
-//     return response.status(400).send(err);
-//   } else {
-//     // db.query('INSERT INTO recipes (recipe_name, continent_name, id) VALUES($1, $2, $3)', [...values], (err, table) => {
-//     db.query('INSERT INTO recipes (recipe_name, ingredients_name, description_name, id) VALUES($1, $2, $3, $4)', [...values], (err, table) => {
-//       done();
-//       if(err) {
-//         return response.status(400).send(err);
-//       } else {
-//         console.log('DATA INSERTED');
-//         response.status(201).send({message: 'Data inserted!'});
-//       }
-//     })
-//   }
-// })
-// })
 
 app.listen(PORT, () => console.log('Listening on port ' + PORT));
 
